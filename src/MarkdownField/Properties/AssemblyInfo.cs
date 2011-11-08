8ed9085da28e54e26e57b60b8018400840136da0
Stephen Pope <stp@sitecore.net>
2011-11-08 12:55:56 +0000

    Updated AssemblyInfo

--- src/MarkdownField/Properties/AssemblyInfo.cs
+++ src/MarkdownField/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MarkdownField")]
+[assembly: AssemblyTitle("Sitecore MarkdownField")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MarkdownField")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Sitecore")]
+[assembly: AssemblyProduct("Sitecore MarkdownField")]
+[assembly: AssemblyCopyright("Copyright © Sitecore 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
